9824eee381a6e5893ec35b9690f8968bc0685839
pragnakalpdev79 <pragnakalp.dev79@gmail.com>
2026-01-30 18:49:10 +0530

    Create,List,Single and Update user functions implemented

--- level1/task1.py
+++ level1/task1.py
@@ -69,25 +69,114 @@ def list_users():
         }),500
     
 @app.route("/users/<uuid>",methods=["GET"])
-def list_users():
+def single_user(uuid):
     if not os.path.exists('users.json'):
         return jsonify({
-            "Error" : "No Users Created yet"
-        }),404
+            "Error" : "Internal Server Error"
+        }),500
     try:
         with open('users.json','r') as u:
             data = json.load(u)
-            try:
-                
+            if not data or not data["users"]:
+                return jsonify({
+                    "Error" : "No users created yet"
+                }),404
+            for i in data["users"]:
+                if i["id"] == uuid:
+                    return i
+            return ({
+                "Error" : "User not found"
+            })
     except: 
         return jsonify({
             "Error" : "Internal Server Error"
         }),500
     
+@app.route("/users/<uuid>",methods=["PATCH"])
+def update_user(uuid):
+    if not os.path.exists('users.json'):
+        return jsonify({
+            "Error" : "Internal Server Error"
+        }),500
+    try:
+        with open('users.json','r+') as u:
+            data = json.load(u)
+            requested_user = request.get_json()
 
+            if not data or not data["users"]:
+                return jsonify({
+                    "Error" : "No users created yet"
+                }),404
+            index = 0
+            for user in data["users"]:
+                print("entered the loop",str(user["id"]) == uuid,not requested_user )
+                if str(user["id"]) == uuid and requested_user: 
+                    #user matched and the body is not empty
+                    print("entered the loop and passed the condition")
+                    print("Old user base :- \n=======================================================================")
+                    for j in data["users"]:
+                        print(j)
+                    #print(data["users"])
+                    user = {key : requested_user[key] if key in requested_user else user[key] for key in user}
+                    u.seek(0)
+                    #print(user)
+                    data["users"][index] = user
+                    print("Updated user base :-\n============================================================ ")
+                    #print(data["users"])
+                    for j in data["users"]:
+                        print(j)
+                    json.dump({"users":data["users"]},u,indent=4)
+                    return jsonify({
+                        "message" : "User Updated Successfully",
+                        "user" : user
+                    })
+                index += 1
 
+            return ({
+                "Error" : "User not found",
+                "type" : f"{type(uuid)}",
+                "req_user" : f"{requested_user}"
+            })
+    except: 
+        return jsonify({
+            "Error" : "Internal Server Error"
+        }),500
 
+@app.route("/users/<uuid>",methods=["DELETE"])
+def delete_user(uuid):
+    if not os.path.exists('users.json'):
+        return jsonify({
+            "Error" : "Internal Server Error"
+        }),500
+    try:
+        with open('users.json','r+') as u:
+            data = json.load(u)
+            requested_user = request.get_json()
 
+            if not data or not data["users"]:
+                return jsonify({
+                    "Error" : "No users created yet"
+                }),404
+            for user in data["users"]:
+                print("entered the loop",str(user["id"]) == uuid,not requested_user )
+                if str(user["id"]) == uuid and requested_user: 
+                    #user matched and the body is not empty
+                    print("entered the loop and passed the condition")
+                    user = {key : requested_user[key] if key in requested_user else user[key] for key in user}
+                    return jsonify({
+                        "message" : "User Updated Successfully",
+                        "user" : user
+                    })
 
+            return ({
+                "Error" : "User not found",
+                "type" : f"{type(uuid)}",
+                "req_user" : f"{requested_user}"
+            })
+    except: 
+        return jsonify({
+            "Error" : "Internal Server Error"
+        }),500
+    
 if __name__ == "__main__":
     app.run(debug=True)
