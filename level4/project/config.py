70f770f9b2b61a80db29fb98069bf48367db275d
pragnakalpdev79 <pragnakalp.dev79@gmail.com>
2026-02-05 11:20:51 +0530

    cli command

--- level4/project/config.py
+++ level4/project/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 URI = os.getenv("URI")
-print(URI)
+#print(URI)
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY','default-dev-key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
